dc35c513c62ae0781576dbc81748e719fc4cbb99
Fernanda Valentin Padrón Hernández <fervalph@192.168.1.98>
2025-11-02 20:43:12 -0600

    Código terminado y correcto para obtener perimetros y areas de las figuras

--- figuras_principal.py
+++ figuras_principal.py
@@ -18,5 +18,30 @@ print("---Procesando Figuras---")
 
 #Iterar cada fila de la variable con los datos de las medidas: df
 for index, row in dfile.iterrows():
-	#Acceder a cada valor en cada columna de la fila
-	print(f"Fila {index}: Figura={row['FIGURA']}, Medida1={row['MEDIDA1']}, Medida2={row['MEDIDA2']}")
+	#Acceder e imprimir cada valor en cada columna de la fila
+	#print(f"Fila {index}: Figura={row['FIGURA']}, Medida1={row['MEDIDA1']}, Medida2={row['MEDIDA2']}")
+	
+	#Guardar Figura, Medida1 y Medida2 en una variable temporal
+	figura = row['FIGURA']
+	medida1 = row['MEDIDA1']
+	medida2 = row['MEDIDA2']
+
+	#Comenzar llamando a cada función dependiento a la figura indicada en el archivo csv
+	
+	#Si figura es "c", llamar función del círculo
+	if figura == "c":
+		#Guardar perimetro y area econtradas
+		perimetro, area = circulo(medida1)
+
+	#Si figura es "t", llamar función del triángulo
+	elif figura == "t":
+		#Guardar perimetro y area econtradas
+		perimetro, area = triangulo(medida1,medida2)
+
+	#Si figura es "r", llamar función del rectangulo
+	elif figura == "r":
+		#Guardar perimetro y area econtradas
+		perimetro, area = rectangulo(medida1,medida2)
+
+print("¡Todas las figuras han sido procesadas con éxito!")
+print("\nFin del código.\n")
