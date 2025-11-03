268ec6e7a59a401a7e38a30f931f03cba412c9af
Fernanda Valentin Padrón Hernández <fervalph@192.168.1.98>
2025-11-02 20:20:16 -0600

    Actualización 1 archivo figuras_principal.py

--- figuras_principal.py
+++ figuras_principal.py
@@ -1,12 +1,22 @@
 #Importar librería pandas para leer el csv
 import pandas as pd
+
 #Importar del archivo funciones.py las funciones a ocupar para las figuras del csv
 from funciones import triangulo, rectangulo, circulo
+
 #Leer y guardar contenido del csv en la variable df
-df = pd.read_csv("Figuras.csv")
+dfile = pd.read_csv("Figuras.csv")
+
+#Matriz perímetros
+perimetros = []
+
+#Matriz áreas
+areas = []
+
 #Mensaje de inicio
 print("---Procesando Figuras---")
+
 #Iterar cada fila de la variable con los datos de las medidas: df
-for index, row in df.iterrows():
+for index, row in dfile.iterrows():
 	#Acceder a cada valor en cada columna de la fila
 	print(f"Fila {index}: FIGURA={row['FIGURA']}, Medida1={row['MEDIDA1']}, Medida2={row['MEDIDA']}")
