b96ee6cc4ee6cb423c08a5f2a820d09731242b56
Fernanda Valentin Padrón Hernández <fervalph@192.168.1.98>
2025-11-02 20:27:22 -0600

    Correción manual del archivo csv

--- figuras_principal.py
+++ figuras_principal.py
@@ -5,7 +5,7 @@ import pandas as pd
 from funciones import triangulo, rectangulo, circulo
 
 #Leer y guardar contenido del csv en la variable df
-dfile = pd.read_csv("Figuras.csv")
+dfile = pd.read_csv("figuras.csv")
 
 #Matriz perímetros
 perimetros = []
@@ -19,4 +19,4 @@ print("---Procesando Figuras---")
 #Iterar cada fila de la variable con los datos de las medidas: df
 for index, row in dfile.iterrows():
 	#Acceder a cada valor en cada columna de la fila
-	print(f"Fila {index}: FIGURA={row['FIGURA']}, Medida1={row['MEDIDA1']}, Medida2={row['MEDIDA']}")
+	print(f"Fila {index}: Figura={row['FIGURA']}, Medida1={row['MEDIDA1']}, Medida2={row['MEDIDA2']}")
