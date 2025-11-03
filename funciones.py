1c76c7db70a84c9bcd73221168716a76568b4a57
Fernanda Valentin Padrón Hernández <fervalph@192.168.1.98>
2025-11-02 20:47:16 -0600

    Actualiación 1 archivo funciones.py

--- funciones.py
+++ funciones.py
@@ -4,27 +4,35 @@
 import math
 
 #Función para el triangulo
-def triangulo():
+def triangulo(medida1,medida2):
 	#Obtener perimetro triangulo
-	perimetro = Medida1 + Medida2
+	perimetro = medida1 + medida2
+
 	#Obtener area triangulo
-	area = (Medida1*Medida2)/2
+	area = (medida1*medida2)/2
+
 	#Regresar ambos valores al archivo .py principal
 	return perimetro, area
+
 #Función para el rectángulo
-def rectangulo():
+def rectangulo(medida1,medida2):
         #Obtener perimetro rectangulo
-        perimetro = 2 * (Medida1 + Medida2)
+        perimetro = 2 * (medida1 + medida2)
+
         #Obtener area rectangulo
-        area = Medida1 * Medida2
+        area = medida1 * medida2
+
         #Regresar ambos valores al archivo .py principal
         return perimetro, area
+
 #Función para el círculo
-def circulo():
+def circulo(medida1):
 #La medida del círculo será tomada como el radio del mismo
         #Obtener perimetro círculo
-        perimetro = 2 * math.pi * Medida1
+        perimetro = 2 * math.pi * medida1
+
         #Obtener area círculo
-        area = math.pi * (Medida1)**2
+        area = math.pi * (medida1)**2
+
         #Regresar ambos valores al archivo .py principal
         return perimetro, area
